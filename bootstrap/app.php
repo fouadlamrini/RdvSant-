222b50a21cbacf533e52dfc8b523e5bcc1c15ecf
fouad lmrini <fouad11.lamrini@gmail.com>
2025-04-29 10:28:56 +0100

    ajouter les middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Middleware\EnsureUserIsAdmin as MiddlewareEnsureUserIsAdmin;
+use App\Http\Middleware\EnsureUserIsDoctor as MiddlewareEnsureUserIsDoctor;
+use App\Http\Middleware\EnsureUserIsPatient as MiddlewareEnsureUserIsPatient;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,8 +16,14 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'doctor' => MiddlewareEnsureUserIsDoctor::class,
+            'patient' => MiddlewareEnsureUserIsPatient::class,
+            'admin' => MiddlewareEnsureUserIsAdmin::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
+
+    
