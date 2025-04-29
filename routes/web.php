ef9beef1784697b2d857e75921a4f3054b054012
fouad lmrini <fouad11.lamrini@gmail.com>
2025-04-29 11:06:49 +0100

    ajouter les middleware

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,65 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
+
+
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::get('/admin/dashboard', [DashboardController::class, 'adminDashboard'])->name('admin.dashboard');
+    Route::get('/admin/users/{id}/toggle-status', [UserController::class, 'toggleStatus'])->name('users.toggleStatus');
+    Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+});
+
+
+Route::middleware(['auth', 'doctor'])->group(function () {
+
+    Route::get('/doctor/dashboard', [DashboardController::class, 'doctorDashboard'])->name('doctor.dashboard');
+    Route::get('/doctor/profile', [ProfileContoller::class, 'show'])->name('doctor.profile');
+    Route::post('/doctor/profile', [ProfileContoller::class, 'update'])->name('doctor.profile.update');
+    Route::get('/doctorShudule', [PagesController::class, 'doctorShudule'])->name('doctorShudule');
+    Route::post('/doctorShudule', [PagesController::class, 'storeSchedule']);
+    Route::get('/doctor/schedule/events', [DisponibilityController::class, 'getScheduleEvents']);
+    Route::put('/disponibility/{id}', [DisponibilityController::class, 'update'])->name('disponibility.update');
+    Route::delete('/disponibility/{id}', [DisponibilityController::class, 'destroy'])->name('disponibility.destroy');
+});
+
+
+
+Route::middleware(['auth', 'patient'])->group(function () {
+    Route::get('/patient/dashboard', [DashboardController::class, 'patientDashboard'])->name('patient.dashboard');
+
+    Route::get('/patient/mes-rendez-vous', [DashboardController::class, 'mesRendezVous'])->name('patient.mesRendezVous');
+    Route::get('/doctor/{doctorId}/appointments', [AppointmentController::class, 'showAvailableSlots'])->name('appointments.showAvailableSlots');
+
+
+    Route::post('/doctor/{doctorId}/appointments', [AppointmentController::class, 'bookAppointment'])->name('appointments.book');
+
+    Route::post('appointments/store', [AppointmentController::class, 'store'])->name('appointments.store');
+
+    Route::get('/appointments/{id}/confirm', [AppointmentController::class, 'confirm'])->name('appointments.confirm');
+    Route::delete('/appointments/{id}', [AppointmentController::class, 'destroy'])->name('appointments.destroy');
+});
+
+
+Route::middleware(['auth'])->group(function () {
+
+
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+});
+
+Route::get('/about', [PagesController::class, 'showAbout'])->name('about');
+
+Route::get('/', [PagesController::class, 'showHome'])->name('home');
+
 Route::get('/login', function () {
     return view('auth/login');
 });
 
+Route::post("/login", [AuthController::class, "login"])->name("login");
+
 Route::get('/signup/doctor', function () {
     return view('auth/SignUpDoctor');
 })->name("signup.doctor");
+Route::post("signup/doctor", [AuthController::class, "RegisterDoctor"])->name("signup.doctor");
 
 Route::get('/signup/patient', function () {
     return view('auth/SignUpPatient');
@@ -28,40 +80,7 @@ Route::get('/signup/patient', function () {
 
 Route::post("signup/patient", [AuthController::class, "RegisterPatient"])->name("signup.patient");
 
-Route::post("signup/doctor", [AuthController::class, "RegisterDoctor"])->name("signup.doctor");
 
 
-Route::post("/login", [AuthController::class, "login"])->name("login");
 
-
-Route::middleware(['auth'])->group(function () {
-    Route::get('/admin/dashboard', [DashboardController::class, 'adminDashboard'])->name('admin.dashboard');
-    Route::get('/doctor/dashboard', [DashboardController::class, 'doctorDashboard'])->name('doctor.dashboard');
-    Route::get('/patient/dashboard', [DashboardController::class, 'patientDashboard'])->name('patient.dashboard');
-    Route::get('/admin/users/{id}/toggle-status', [UserController::class, 'toggleStatus'])->name('users.toggleStatus');
-    Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
-    Route::get('/patient/mes-rendez-vous', [DashboardController::class, 'mesRendezVous'])->name('patient.mesRendezVous');
-    Route::get('/doctor/profile', [ProfileContoller::class, 'show'])->name('doctor.profile');
-    Route::post('/doctor/profile', [ProfileContoller::class, 'update'])->name('doctor.profile.update');
-});
-
-Route::get('/about',[PagesController::class, 'showAbout'])->name('about');
-Route::get('/',[PagesController::class, 'showHome'])->name('home');
-Route::get('/doctorShudule',[PagesController::class, 'doctorShudule'])->name('doctorShudule');
-Route::post('/doctorShudule', [PagesController::class, 'storeSchedule']);
-Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
-
-Route::get('/doctor/schedule/events', [DisponibilityController::class, 'getScheduleEvents']);
-Route::put('/disponibility/{id}', [DisponibilityController::class, 'update'])->name('disponibility.update');
-Route::delete('/disponibility/{id}', [DisponibilityController::class, 'destroy'])->name('disponibility.destroy');
-
-Route::get('/doctor/{doctorId}/appointments', [AppointmentController::class, 'showAvailableSlots'])->name('appointments.showAvailableSlots');
-
-
-Route::post('/doctor/{doctorId}/appointments', [AppointmentController::class, 'bookAppointment'])->name('appointments.book');
-
-Route::post('appointments/store', [AppointmentController::class, 'store'])->name('appointments.store');     
-
-Route::get('/appointments/{id}/confirm', [AppointmentController::class, 'confirm'])->name('appointments.confirm');
-Route::delete('/appointments/{id}', [AppointmentController::class, 'destroy'])->name('appointments.destroy');
 
